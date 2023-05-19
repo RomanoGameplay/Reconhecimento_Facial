fe2306386f1e9608bd383486c5a76bc5f8f747d1
RomanoGameplay <82478812+RomanoGameplay@users.noreply.github.com>
2023-05-19 12:43:34 -0300

    Mudança o nome "face_teste" da pasta que contém o programa para "aplicação".

--- Aplicação/InterfaceMain.py
+++ Aplicação/InterfaceMain.py
@@ -88,86 +88,91 @@ class InterfaceMain:
                 continue
 
     def _options(self, settings, schedules):
-        obs = '   |__ OBS: Uma vez fechada a página, necessita-se de reiniciar o programa para acessá-la novamente!'
-        configurations = settings.load_settings()
-        images = Images(path=configurations['path'])
-        schedules_data = schedules.load()
-        morning_hours = schedules_data['horario_manha']
-        afternoon_hours = schedules_data['horario_tarde']
-        morning_hours = [datetime.strptime(morning_hours[0], '%H:%M'), datetime.strptime(morning_hours[1], '%H:%M')]
-        afternoon_hours = [datetime.strptime(afternoon_hours[0], '%H:%M'),
-                           datetime.strptime(afternoon_hours[1], '%H:%M')]
+        try:
+            obs = '   |__ OBS: Uma vez fechada a página, necessita-se de reiniciar o programa para acessá-la novamente!'
+            configurations = settings.load_settings()
+            images = Images(path=configurations['path'])
+            schedules_data = schedules.load()
+            morning_hours = schedules_data['horario_manha']
+            afternoon_hours = schedules_data['horario_tarde']
+            morning_hours = [datetime.strptime(morning_hours[0], '%H:%M'), datetime.strptime(morning_hours[1], '%H:%M')]
+            afternoon_hours = [datetime.strptime(afternoon_hours[0], '%H:%M'),
+                               datetime.strptime(afternoon_hours[1], '%H:%M')]
 
-        while True:
-            self._title()
-            print('1 - Reconhecimento Facial! (Aperte "esc" para sair do reconhecimento facial!)')
-            print('2 - Cadastrar alunos! (Requer conexão com a internet)\n', obs)
-            print('3 - Baixar imagens do Gmail! (Requer conexão com a internet)')
-            print('4 - Deletar imagens na pasta {}!'.format(configurations['path']))
-            print('5 - Relatórios dos alunos! (Requer conexão com internet)\n', obs)
-            print('6 - Alterar configurações!')
-            print('7 - Alterar horários!')
-            print('8 - Importar backups salvos do Banco de Dados!')
-            print('9 - Deletar os dados do Banco de Dados!')
-            print('"{}" - Sair!'.format(self._key_exit))
+            while True:
+                self._title()
+                print('1 - Reconhecimento Facial! (Aperte "esc" para sair do reconhecimento facial!)')
+                print('2 - Cadastrar alunos! (Requer conexão com a internet)\n', obs)
+                print('3 - Baixar imagens do Gmail! (Requer conexão com a internet)')
+                print('4 - Deletar imagens na pasta {}!'.format(configurations['path']))
+                print('5 - Relatórios dos alunos! (Requer conexão com internet)\n', obs)
+                print('6 - Alterar configurações!')
+                print('7 - Alterar horários!')
+                print('8 - Importar backups salvos do Banco de Dados!')
+                print('9 - Deletar os dados do Banco de Dados!')
+                print('"{}" - Sair!'.format(self._key_exit))
 
-            input_option = input('\nDigite uma das opções acima: ')
+                input_option = input('\nDigite uma das opções acima: ')
 
-            if input_option == '1':
-                facial_recognition = FacialRecognition(path=configurations['path'], morning_hours=morning_hours,
-                                                       afternoon_hours=afternoon_hours)
-                facial_recognition.run(table_missings=self.table_missings, table_presents=self.table_presents,
-                                       table_registered=self.table_registered)
-                if not os.path.exists('Backup_DataBase'):
-                    os.makedirs('Backup_DataBase')
-                self._backup_database()
-                break
-            elif input_option == '2':
-                if self._check_internet():
-                    web_formulary = Formulary()
-                    web_formulary.main(self.table_registered, debug=False)
+                if input_option == '1':
+                    facial_recognition = FacialRecognition(path=configurations['path'], morning_hours=morning_hours,
+                                                           afternoon_hours=afternoon_hours)
+                    facial_recognition.run(table_missings=self.table_missings, table_presents=self.table_presents,
+                                           table_registered=self.table_registered)
+                    if not os.path.exists('Backup_DataBase'):
+                        os.makedirs('Backup_DataBase')
+                    self._backup_database()
+                    break
+                elif input_option == '2':
+                    if self._check_internet():
+                        web_formulary = Formulary()
+                        web_formulary.main(self.table_registered, debug=False)
+                        continue
                     continue
-                continue
-            elif input_option == '3':
-                if self._check_internet():
-                    gmail = GetImagesFromGmail(user=configurations['email_user_gmail'],
-                                               subject=configurations['subject_gmail'], path=configurations['path'],
-                                               gmail_access=configurations['gmail_access'])
-                    gmail.main(self.table_registered)
-                    images.resize_images()
+                elif input_option == '3':
+                    if self._check_internet():
+                        gmail = GetImagesFromGmail(user=configurations['email_user_gmail'],
+                                                   subject=configurations['subject_gmail'], path=configurations['path'],
+                                                   gmail_access=configurations['gmail_access'])
+                        gmail.main(self.table_registered)
+                        images.resize_images()
+                        continue
                     continue
-                continue
-            elif input_option == '4':
-                images.delete_all_images()
-                continue
-            elif input_option == '5':
-                if self._check_internet():
-                    web_relatory = Relatory(table_presents=self.table_presents, table_registered=self.table_registered,
-                                            table_missings=self.table_missings)
-                    web_relatory.main(debug=False)
+                elif input_option == '4':
+                    images.delete_all_images()
                     continue
-                continue
-            elif input_option == '6':
-                settings.main()
-                continue
-            elif input_option == '7':
-                schedules.main()
-                continue
-            elif input_option == '8':
-                self._import_backup_database()
-                continue
-            elif input_option == '9':
-                delete_data = input(
-                    'Você tem certeza?\nEssa ação apagará todos os dados existentes nso bancos de dados!zn\t[S | N]: ')
-                if delete_data.upper() == 'S':
-                    self._delete_database()
-                    print('\nDados apagados!\n')
-                continue
-            elif input_option == self._key_exit:
-                break
-            else:
-                print('\nEscolha apenas a opção com o número correspondente!\n')
-                continue
+                elif input_option == '5':
+                    if self._check_internet():
+                        web_relatory = Relatory(table_presents=self.table_presents, table_registered=self.table_registered,
+                                                table_missings=self.table_missings)
+                        web_relatory.main(debug=False)
+                        continue
+                    continue
+                elif input_option == '6':
+                    settings.main()
+                    continue
+                elif input_option == '7':
+                    schedules.main()
+                    continue
+                elif input_option == '8':
+                    self._import_backup_database()
+                    continue
+                elif input_option == '9':
+                    delete_data = input(
+                        'Você tem certeza?\nEssa ação apagará todos os dados existentes nso bancos de dados!zn\t[S | N]: ')
+                    if delete_data.upper() == 'S':
+                        self._delete_database()
+                        print('\nDados apagados!\n')
+                    continue
+                elif input_option == self._key_exit:
+                    break
+                else:
+                    print('\nEscolha apenas a opção com o número correspondente!\n')
+                    continue
+        except KeyboardInterrupt:
+            print('Programa parado de forma forçada!')
+        except Exception as e:
+            print(e)
 
     def main(self):
         try:
